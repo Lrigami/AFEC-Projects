2b3f22a8fde6735f5c3230cec7084b91a6a28964
Lrigami <prune.thiry@hotmail.com>
2024-11-20 14:29:40 +0100

    add some comments

--- AwaleGame/script.js
+++ AwaleGame/script.js
@@ -1,19 +1,24 @@
-let cells = document.querySelectorAll("td"); // get all cells
-let cellOne = document.getElementById("cell1");
-let cellTwelve = document.getElementById("cell12");
+// const 
+const cells = document.querySelectorAll("td"); // get all cells
+const cellOne = document.getElementById("cell1");
+const cellTwelve = document.getElementById("cell12");
 
-const initialiseGame = () => { // initialisation to 4 in each cell (start)
+// Function to initialise the game with 4 in each cell
+const initialiseGame = () => {
   cells.forEach((cell) => { 
     cell.value = 4;
     cell.innerText = cell.value;
   })
 }
-initialiseGame();
 
+initialiseGame(); // call the function to initialise the game
+
+// Check the value of the cell
 const getValueFromCell = (event) => {
     return event.target.value;
 }    
 
+// Return if the value is under twelve
 const isValueUnderTwelve = (event) => {
     if (getValueFromCell(event) <= 11) {
         return true;
@@ -22,37 +27,33 @@ const isValueUnderTwelve = (event) => {
     }
 }
 
-const distributeValue = (cell) => {
+// Function to distribute the value until eleven (for the case when the cell value is sup to 12)
+const distributeUntilEleven = (cell) => {
+    let nextCell = cell.nextElementSibling;
+    let cellValueStart = cell.value;
 
-    if (isValueUnderTwelve(event)) {
-        distributeToNext(cell);
-    } else {
-        let nextCell = cell.nextElementSibling;
-        let cellValueStart = cell.value;
+    for (let i = 11; i > 0; i--) {
+        cell.value--;
+        cell.innerText = cell.value;
 
-        for (let i = 11; i > 0; i--) {
-            cell.value--;
-            cell.innerText = cell.value;
-    
-            if(!cell.nextElementSibling && i == cellValueStart) {
-                nextCell = cellOne;
-            } else if(cell.nextElementSibling && i == cellValueStart) {
-                nextCell = cell.nextElementSibling;
-            } 
-    
-            nextCell.value++;
-            nextCell.innerText = nextCell.value;
-    
-            if(!nextCell.nextElementSibling) {
-                nextCell = cellOne;
-            } else {
-                nextCell = nextCell.nextElementSibling;
-            }
+        if(!cell.nextElementSibling && i == cellValueStart) {
+            nextCell = cellOne;
+        } else if(cell.nextElementSibling && i == cellValueStart) {
+            nextCell = cell.nextElementSibling;
+        } 
+
+        nextCell.value++;
+        nextCell.innerText = nextCell.value;
+
+        if(!nextCell.nextElementSibling) {
+            nextCell = cellOne;
+        } else {
+            nextCell = nextCell.nextElementSibling;
         }
-        distributeToPrevious(cell);
     }
 }
 
+// distribute the cell value one by one to the next cells
 const distributeToNext = (cell) => {
     let nextCell = cell.nextElementSibling;
     let cellValueStart = cell.value;
@@ -78,6 +79,7 @@ const distributeToNext = (cell) => {
     }
 }
 
+// distribute all the remainng value of the cell to the previous cell
 const distributeToPrevious = (cell) => {
     let previousCell = cell.previousElementSibling;
     let cellValueStart = cell.value;
@@ -97,6 +99,17 @@ const distributeToPrevious = (cell) => {
     }
 }
 
+// distribute the value depending on the case (call the right functions)
+const distributeValue = (cell) => {
+    if (isValueUnderTwelve(event)) {
+        distributeToNext(cell);
+    } else {
+        distributeUntilEleven(cell);
+        distributeToPrevious(cell);
+    }
+}
+
+// click EventListener on the cell when the user plays
 cells.forEach((cell) => {
     cell.addEventListener("click", (event) => {
         distributeValue(cell);
