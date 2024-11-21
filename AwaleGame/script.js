92a15c08e3ec857f5bd723adec18a6cc1f40f369
Lrigami <prune.thiry@hotmail.com>
2024-11-21 10:26:09 +0100

    collect of the seeds and manage player turns

--- AwaleGame/script.js
+++ AwaleGame/script.js
@@ -1,14 +1,27 @@
-// const declarations.
+// declarations.
 const cells = document.querySelectorAll("td");
 const cellOne = document.getElementById("cell1");
 const cellTwelve = document.getElementById("cell12");
 
+let currentPlayer = 1;
+const turnParagraph = document.getElementById("turn");
+const playerBoardOne = document.getElementById("player-board-1");
+const playerBoardTwo = document.getElementById("player-board-2");
+let playerBoard = document.getElementById(`player-board-${currentPlayer}`);
+
+let lastCell;
+
 // Function to initialise the game with 4 in each cell.
 const initialiseGame = () => {
-  cells.forEach((cell) => { 
-    cell.value = 4;
-    cell.innerText = cell.value;
-  })
+    turnParagraph.innerText = `It is player ${currentPlayer} turn.`;
+    cells.forEach((cell) => { 
+        cell.value = 4;
+        cell.innerText = cell.value;
+    })
+    playerBoardOne.value = 0;
+    playerBoardTwo.value = 0;
+    playerBoardOne.innerText = playerBoardOne.value;
+    playerBoardTwo.innerText = playerBoardTwo.value;
 }
 
 // call the function to initialise the game.
@@ -37,19 +50,55 @@ const distributeToNext = (cell) => {
         nextCell.value++;
         nextCell.innerText = nextCell.value;
 
-        if(!nextCell.nextElementSibling) { // change the next cell to be the cell after this next one.
-            nextCell = cellOne; // if it doesn't have a sibling, the default next cell is cellOne.
-        } else if (nextCell.nextElementSibling == cell) {
-            nextCell = cell.nextElementSibling;
+        if (i !== 1) {
+            if(!nextCell.nextElementSibling) { // change the next cell to be the cell after this next one.
+                nextCell = cellOne; // if it doesn't have a sibling, the default next cell is cellOne.
+            } else if (nextCell.nextElementSibling == cell) {
+                nextCell = cell.nextElementSibling;
+            } else {
+                nextCell = nextCell.nextElementSibling; // it it does, the next cell is the next sibling.
+            }
         } else {
-            nextCell = nextCell.nextElementSibling; // it it does, the next cell is the next sibling.
+            lastCell = nextCell;
+            return lastCell;
         }
     }
+}
+
+// collect of the seeds
+const isSeedsEqualToTwoOrThree = (cell) => {
+    if (cell.value == 2 || cell.value == 3) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+const collectSeeds = (lastCell) => {
+    while (isSeedsEqualToTwoOrThree(lastCell)) {
+        playerBoard.value += lastCell.value;
+        playerBoard.innerText = playerBoard.value;    
+        lastCell.value = 0;
+        lastCell.innerText = lastCell.value;
+        lastCell = lastCell.previousElementSibling;
+    }
+    return;
+}
+
+// vérifier que la valeur de la cell = 2 ou 3. si oui -> collecter + vérifier la cellule précédente, sinon stop
+
+// Manage the players turns
+const newTurn = () => {
+    currentPlayer = currentPlayer === 1 ? 2 : 1;
+    turnParagraph.innerText = `It is player ${currentPlayer} turn.`;
 }
 
 // click EventListener on the cell when the user plays.
 cells.forEach((cell) => {
     cell.addEventListener("click", (event) => {
         distributeToNext(cell);
+        collectSeeds(lastCell);
+        newTurn();
     })
 })
