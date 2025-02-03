259e94b0d834ae80556ac38aff53327861ccfcf0
Lrigami <prune.thiry@hotmail.com>
2025-02-03 10:34:52 +0100

    doc swagger

--- OpenAPI_Documentation/server.js
+++ OpenAPI_Documentation/server.js
@@ -1,6 +1,24 @@
-let express = require(' express ');
-let app = express();
+const express = require('express');
+const app = express();
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.get('/api/hello', function(req, res) {
-    res.send("Tout est ok")
+    res.send("Tout est ok");
+});
+
+app.get('/users', function(req, res) {
+    res.send("users ok");
+})
+
+app.get('/users/:id', function(req, res) {
+    console.log("ok");
+    res.send(req.params.id);
+})
+
+app.listen(3000, () => {
+    console.log('Serveur démarré sur http://localhost:3000');
+    console.log('Documentation available on http://localhost:3000/api-docs')
 });
