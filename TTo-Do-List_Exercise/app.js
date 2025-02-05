03b00ccb5ea82f4d3b1fece1d53d6e311819ce05
Lrigami <prune.thiry@hotmail.com>
2025-02-05 10:20:40 +0100

    add config

--- TTo-Do-List_Exercise/app.js
+++ TTo-Do-List_Exercise/app.js
@@ -1,24 +1,19 @@
 // To-Do-List Exercise 
 require('dotenv').config();
 const express = require('express');
-const mongoose = require('mongoose');
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('./swagger.json');
 const taskRoutes = require('./routes/taskRoutes');
+const connectDB = require('./config/config');
 const port = 3000;
 const app = express();
+connectDB();
 
 app.use(express.json());
 app.use('/api', taskRoutes);
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
-mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlPaser: true,
-    useUnifiedTypology: true
-}).then(() => {
-    console.log("MongoDB connected");
-    app.listen(PORT, () => {
-        console.log(`Server started on http//:localhost:${port}`);
-        console.log(`Documentation available on http://localhost:${port}/api-docs`);
-    });
-}).catch(err => console.log(err));
+app.listen(port, () => {
+    console.log(`Server started on http//:localhost:${port}`);
+    console.log(`Documentation available on http://localhost:${port}/api-docs`);
+});
