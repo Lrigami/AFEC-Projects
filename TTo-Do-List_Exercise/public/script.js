3b83a0a0e7be1d653de04acc75e3a900f9724d5f
Lrigami <prune.thiry@hotmail.com>
2025-02-07 13:55:34 +0100

    add basic const for further manips

---
+++ TTo-Do-List_Exercise/public/script.js
@@ -0,0 +1,14 @@
+// Parameters
+const displayedTaskNbr = document.getElementById("displayed-task-nbr");
+
+// Title
+const toDoListTitle = document.getElementById("list-title");
+const modifyTitleBtn = document.getElementById("modify-title");
+const changeTitleInputLabel = document.getElementById("change-title-label");
+const changeTitleInput = document.getElementById("change-title");
+
+// List of tasks
+const tasksList = document.getElementById("tasks-list");
+
+// Actions
+const addNewTask = document.getElementById("add-new-task");
