c0b940eed5e635667f4411d556789c33534e249c
Lrigami <prune.thiry@hotmail.com>
2025-02-07 15:38:50 +0100

    change to-do list title logic

--- TTo-Do-List_Exercise/public/script.js
+++ TTo-Do-List_Exercise/public/script.js
@@ -6,8 +6,9 @@ const displayedTaskNbr = document.getElementById("displayed-task-nbr");
 // Title
 const toDoListTitle = document.getElementById("list-title");
 const modifyTitleBtn = document.getElementById("modify-title");
-const changeTitleInputLabel = document.getElementById("change-title-label");
+const changeTitleInterface = document.getElementById("change-title-interface");
 const changeTitleInput = document.getElementById("change-title");
+const validateTitle = document.getElementById("validate-new-title");
 
 // List of tasks
 const tasksList = document.getElementById("tasks-list");
@@ -18,6 +19,19 @@ const newTaskInterface = document.getElementById("add-new-task-interface");
 const taskTitle = document.getElementById("task-title");
 const taskDescription = document.getElementById("task-description");
 const validate = document.getElementById("create-task");
+
+// Modify the to-do list title
+modifyTitleBtn.addEventListener("click", () => changeTitleInterface.style.display = "block")
+
+changeTitleInterface.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    let title = changeTitleInput.value;
+    toDoListTitle.innerText = title;
+
+    changeTitleInput.value = "";
+    changeTitleInterface.style.display = "none";
+} )
 
 // Create a new task
 addNewTask.addEventListener("click", () => newTaskInterface.style.display = "inline-grid");
