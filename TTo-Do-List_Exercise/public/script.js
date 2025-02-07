ae91c6ecfa1fad501827c70bd96656d1bc3aa39a
Lrigami <prune.thiry@hotmail.com>
2025-02-07 14:50:02 +0100

    create a new task logic

--- TTo-Do-List_Exercise/public/script.js
+++ TTo-Do-List_Exercise/public/script.js
@@ -1,4 +1,6 @@
+// Const
 // Parameters
+const port = window.location.port || 3000;
 const displayedTaskNbr = document.getElementById("displayed-task-nbr");
 
 // Title
@@ -10,5 +12,33 @@ const changeTitleInput = document.getElementById("change-title");
 // List of tasks
 const tasksList = document.getElementById("tasks-list");
 
-// Actions
+// New task
 const addNewTask = document.getElementById("add-new-task");
+const newTaskInterface = document.getElementById("add-new-task-interface");
+const taskTitle = document.getElementById("task-title");
+const taskDescription = document.getElementById("task-description");
+const validate = document.getElementById("create-task");
+
+// Create a new task
+addNewTask.addEventListener("click", () => newTaskInterface.style.display = "inline-grid");
+
+newTaskInterface.addEventListener("submit", async (event) => {
+    event.preventDefault();
+
+    const title = taskTitle.value;
+    const description = taskDescription.value.trim() || "";
+
+    if(!title) {
+        alert("A title is required.");
+        return;
+    }
+
+    const response = await fetch(`http://localhost:${port}/api/tasks`, {
+        method: "POST", 
+        headers: {"Content-Type": "application/json"}, 
+        body: JSON.stringify({title, description})
+    });
+
+    const result = await response.json();
+    console.log(result);
+})
